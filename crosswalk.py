a512c22a5622837b18a3527f456b04d585d6ac2e
gusguyman <mkorenfhspltw09@gmail.com>
2017-05-31 16:45:31 -0700

    Switched to ZeroBaseline

--- crosswalk.py
+++ crosswalk.py
@@ -1,5 +1,6 @@
 from rllab.algos.trpo import TRPO
 from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.baselines.zero_baseline import ZeroBaseline
 from rllab.envs.crosswalk_env import CrosswalkEnv
 from rllab.envs.normalized_env import normalize
 from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
@@ -9,6 +10,7 @@ policy = GaussianMLPPolicy(env_spec=env.spec)
 baseline = LinearFeatureBaseline(env_spec=env.spec)
 algo = TRPO(
     env=env,
-    policy=policy
+    policy=policy,
+    baseline=ZeroBaseline(env.spec)
 )
 algo.train()
