bf09c009721c22fdc9b1047fd3ddcd9cec67f948
gusguyman <mkorenfhspltw09@gmail.com>
2017-05-31 17:51:07 -0700

    Tweaked hyperparams

--- crosswalk.py
+++ crosswalk.py
@@ -7,12 +7,14 @@ from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
 
 env = normalize(CrosswalkEnv())
 policy = GaussianMLPPolicy(env_spec=env.spec,
-                           hidden_sizes=(256, 128, 64, 32))
+                           hidden_sizes=(512, 256, 128, 64, 32))
 baseline = LinearFeatureBaseline(env_spec=env.spec)
 algo = TRPO(
     env=env,
     policy=policy,
     baseline=ZeroBaseline(env.spec),
-    batch_size=4000
+    batch_size=4000,
+    step_size=0.001,
+    n_itr=1000
 )
 algo.train()
